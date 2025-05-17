1b5d5e505602232dd25eecbfa30d6fdfd457d3eb
SurajGude1 <surajgude2001@gmail.com>
2025-05-17 10:53:17 +0530

    working...

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import styles from "./styles/not-found.module.css";
 import Logo from "../../public/logo/logo";
+import Button from "./components/global/buttons/button";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push("/");
+  };
+
   return (
     <div className={styles.NotFound}>
-      <Logo />
-      <h1 className={styles.PageTitle}>Page not found...</h1>
+      <div className={styles.Content}>
+        <Logo />
+        <h1 className={styles.PageTitle}>Page not found</h1>
+        <Button
+          text="Go to home"
+          backgroundColor="var(--charcoal"
+          hoverEffect={false}
+          onClick={handleGoHome}
+        />
+      </div>
     </div>
   );
 }
